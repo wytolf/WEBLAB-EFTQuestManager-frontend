2cee77c55c47c9b1f9ef0a6086dbaed1374b1ab7
Matthias Brunner <brunner.matthias@outlook.com>
2024-03-18 22:50:15 +0100

    refactor: added tests

--- src/app/auth/register/register.component.spec.ts
+++ src/app/auth/register/register.component.spec.ts
@@ -1,17 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegisterComponent } from './register.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let authService: jasmine.SpyObj<AuthService>;
+  let router: Router;
 
   beforeEach(async () => {
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['register']);
+
     await TestBed.configureTestingModule({
-      declarations: [RegisterComponent]
-    })
-    .compileComponents();
-    
+      declarations: [RegisterComponent],
+      imports: [ReactiveFormsModule],
+      providers: [{ provide: AuthService, useValue: authServiceSpy }, FormBuilder, Router]
+    }).compileComponents();
+
+    authService = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
+    router = TestBed.inject(Router);
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -20,4 +32,29 @@ describe('RegisterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should initialize the form properly', () => {
+    expect(component.form).toBeDefined();
+    expect(component.form.get('email')).toBeTruthy();
+    expect(component.form.get('role')).toBeTruthy();
+    expect(component.form.get('password')).toBeTruthy();
+  });
+
+  it('should call register method of AuthService and navigate to manager when register is called', () => {
+    const userData = {
+      email: 'test@example.com',
+      role: 'user',
+      password: 'password123'
+    };
+    const emptyPromise: Promise<any> = Promise.resolve();
+    authService.register.and.returnValue(emptyPromise);
+    const navigateSpy = spyOn(router, 'navigate');
+
+    component.form.setValue(userData);
+    component.register();
+
+    expect(authService.register).toHaveBeenCalledWith(userData);
+    expect(navigateSpy).toHaveBeenCalledWith(['manager']);
+  });
 });
+
